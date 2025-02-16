bafb2bb0a1164afbbf5334aa3f59d745cdd67315
Andrei Enache <andreienake@gmail.com>
2025-02-16 07:41:16 +0100

    Made DX static to increase performance

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use atomic_float::AtomicF64;
 static INTERVALS: u32 = 10000000;
 static NUM_THREADS: u32 = 7;
 static PI25DT: f64 = 3.141592653589793238462643;
+static DX: f64 = 1.0 / INTERVALS as f64;
 
 fn main() {
     let iterations = 100;
@@ -13,14 +14,14 @@ fn main() {
         let now = Instant::now();
         let sum = &AtomicF64::new(0.0);
         let step = INTERVALS / NUM_THREADS;
-        let dx = 1.0 / INTERVALS as f64;
         
         thread::scope(|s| {
             for i in 0..NUM_THREADS {
-                s.spawn(move || sum.fetch_add(par_sum(i*step + 1, (i+1)*step + 1, dx), Relaxed));
+                s.spawn(move || sum.fetch_add(par_sum(i*step + 1, (i+1)*step + 1), Relaxed));
             }
         });
-        let pi = dx * (sum.load(Relaxed) + par_sum(NUM_THREADS*step + 1, NUM_THREADS*step + 1 + INTERVALS % NUM_THREADS, dx));
+        let rem = par_sum(NUM_THREADS*step + 1, NUM_THREADS*step + 1 + INTERVALS % NUM_THREADS);
+        let pi = DX * (sum.load(Relaxed) + rem);
     
         println!("Iteration {i}");
         println!("NUM_THREADS = {:?}", NUM_THREADS);
@@ -35,10 +36,10 @@ fn main() {
     println!("Average time: {:?}", total / iterations)
 }
 
-fn par_sum(start_interval: u32, end_interval: u32, dx: f64) -> f64 {
+fn par_sum(start_interval: u32, end_interval: u32) -> f64 {
     let mut sum = 0.0;
-    for i in start_interval..end_interval  {
-        let x = dx * (i as f64 - 0.5);
+    for i in start_interval..end_interval {
+        let x = DX * (i as f64 - 0.5);
         let f = 4.0 / (1.0 + x*x);
         sum += f;
     }
